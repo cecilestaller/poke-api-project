2b9ec983b4bdd444690309c5445ae981122735a7
cecilestaller <c.staller96@gmail.com>
2023-12-07 12:02:28 +0100

    added ordner

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Type from './pages/Type'
-import Detail from './pages/Detail'
+import Home from './pages/home/Home'
+import Type from './pages/type/Type'
+import Detail from './pages/detail/Detail'
 import { FetchContextProvider } from './context/Context'
 import PokeFetch from './data/PokeFetch'
 
