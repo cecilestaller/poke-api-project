4e51646fce9adc02cbe571bf6e8d73797661e7ab
wilhelm-lenz <wilhelm.v.lenz@gmail.com>
2023-12-07 15:52:43 +0100

    refactoring dev

--- src/data/PokeFetch.jsx
+++ src/data/PokeFetch.jsx
@@ -3,26 +3,25 @@ import { FetchContext } from "../context/Context";
 
 const PokeFetch = () => {
   const { pokeData, setPokeData } = useContext(FetchContext);
-  const { pokeTypes, setPokeTypes } = useContext(FetchContext);
 
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon")
       .then((res) => res.json())
       .then((data) => {
         let newArr = [];
-                data.results?.map((singlePokeObj) => {
-                    fetch(`${singlePokeObj.url}`)
-                        .then(res => res.json())
-                        .then(singleData => {
-                            newArr.push({...singleData})
-                            setPokeData(newArr)
-                        })
-                })
-            })
-            .catch(error => console.log(error))
-    }, [])
-    // console.log('final poke log: ', pokeData);
-}
+
+        data.results?.map((singlePokeObj) => {
+          fetch(`${singlePokeObj.url}`)
+            .then((res) => res.json())
+            .then((singleData) => {
+              newArr.push({ ...singleData });
+              setPokeData(newArr);
+            });
+        });
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/type")
       .then((response) => response.json())
