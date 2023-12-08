26c95ddcd9cf99c3524501166a453ef2b501b315
SuperCoderSuat <supercodesuat@gmail.com>
2023-12-08 14:11:32 +0100

    bug fixed

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,35 +3,32 @@ import BurgerMenuIcon from "../svg/BurgerMenuIcon";
 import DarkModeIcon from "../svg/DarkModeIcon";
 import "./Nav.scss";
 import { useContext, useEffect, useState } from "react";
-import { FetchContext } from "../../context/Context";
+import { FetchContext } from "../../context/Context"
 
 const Nav = () => {
-  const { searchItem, setSearchItem } = useContext(FetchContext);
+    
+    const { searchItem, setSearchItem } = useContext(FetchContext);
 
-  const handleSearch = (e) => {
-    setSearchItem(e.target.value);
-  };
+    const handleSearch = (e) => {
+        setSearchItem(e.target.value);
+    }
 
-  useEffect(() => {
-    console.log(searchItem);
-  }, [searchItem]);
-
-  return (
-    <nav>
-      <Link to="/type">
-        <BurgerMenuIcon />
-      </Link>
-      <input
-        type="text"
-        placeholder="Search Pokemon"
-        value={searchItem}
-        onChange={handleSearch}
-      />
-      <Link>
-        <DarkModeIcon />
-      </Link>
-    </nav>
-  );
-};
+    return (  
+        <nav>
+            <Link to="/type">
+                <BurgerMenuIcon />
+            </Link>
+            <input
+                type="text"
+                placeholder="Search Pokemon"
+                value={searchItem}
+                onChange={handleSearch}
+                />
+            <Link>
+                <DarkModeIcon />
+            </Link>
+        </nav>
+    );
+}
 
 export default Nav;
